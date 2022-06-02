6727870f3a9d05e1151189da5e75ca5c81f7d8d6
mr-ptw5 <wrightptw@gmail.com>
2022-06-02 17:56:50 -0500

    counting divisors for a number, level 7 easy

---
+++ Count divisors.js
@@ -0,0 +1,10 @@
+function getDivisorsCnt(n){
+  let count = 0
+  for (let i = 1; i <= n; i++) {
+      if (n % i === 0)
+        count++
+    }
+  return count
+}
+
+// https://www.codewars.com/kata/542c0f198e077084c0000c2e
