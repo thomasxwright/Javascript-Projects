d5645515d425de7d6d7cfe50cb5c26a6b59e775d
mr-ptw5 <wrightptw@gmail.com>
2022-06-28 21:07:58 -0500

    powers of two easy lev 8

---
+++ powers-of-two.js
@@ -0,0 +1,9 @@
+function powersOfTwo(n){
+  const result = []
+  for (let i = 0; i <= n; i++) {
+    result.push(2 ** i)
+  }
+  return result
+}
+
+// https://www.codewars.com/kata/57a083a57cb1f31db7000028
