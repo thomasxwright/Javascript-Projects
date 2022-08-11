7eeefae50fb91b38e04b2058bec5563a1f0dd969
mr-ptw5 <wrightptw@gmail.com>
2022-08-10 21:37:03 -0500

    sheep counting lev 8

---
+++ sheep-counter.js
@@ -0,0 +1,11 @@
+var countSheep = function (num){
+	  let utterance = ''
+	  let counter = 0
+	  
+	  while (counter < num) {
+		      utterance += `${++counter} sheep...`
+		    }
+	  return utterance
+}
+
+// https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
