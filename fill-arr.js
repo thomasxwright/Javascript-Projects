3afe3b346a26d14cda7dfdb27f60780d2e9b0683
thomasxwright <wrightptw@gmail.com>
2022-09-14 18:54:22 -0500

    fill array lev 8

---
+++ fill-arr.js
@@ -0,0 +1,9 @@
+const arr = N => {
+	  const result = []
+	  for (let i = 0; i < N; i++) {
+		      result.push(i)
+		    }
+	  return result
+}
+
+// https://www.codewars.com/kata/571d42206414b103dc0006a1
