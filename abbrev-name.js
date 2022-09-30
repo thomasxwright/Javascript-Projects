4b956319caea44c312d90c41985ea9c260957897
thomasxwright <wrightptw@gmail.com>
2022-09-30 15:07:33 -0500

    abbrev name lev 7 and dna to rna lev 8

---
+++ abbrev-name.js
@@ -0,0 +1,5 @@
+function abbrevName(name){
+	  return name.split(' ').map(name => name[0].toUpperCase()).join('.')
+}
+
+// https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
---
+++ dna-to-rna.js
@@ -0,0 +1,5 @@
+function DNAtoRNA(dna) {
+	  return dna.replace(/T/g, 'U')
+}
+
+// https://www.codewars.com/kata/5556282156230d0e5e000089
