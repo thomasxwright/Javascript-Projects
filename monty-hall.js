9953cf1dac00df8352abd121e3a12f3bd9ca7c57
thomasxwright <wrightptw@gmail.com>
2022-11-16 23:06:43 -0600

    monty hall explained

--- monty-hall.js
+++ monty-hall.js
@@ -1,4 +1,5 @@
 let montyHall = (correctDoorNumber, participantGuesses) => Math.round(100 * participantGuesses.filter(guess => guess !== correctDoorNumber).length / participantGuesses.length)
 
+//monty hall problem is the commonly misunderstood statistics game where one door of three contains a prize. after picking one door, one of the two remaining doors is revealed as false, and the player is then given the option to swap again.
 
 // https://www.codewars.com/kata/54f9cba3c417224c63000872
