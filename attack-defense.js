478088ddc1c8a0c8e5f9822b1297f42bc3f6fe40
thomasxwright <wrightptw@gmail.com>
2022-12-09 21:42:22 -0600

    attack defense lev 7

---
+++ attack-defense.js
@@ -0,0 +1,19 @@
+
+n hasSurvived(attackers, defenders){
+	  const sharedSize = Math.min(attackers.length, defenders.length)
+	  let attackersScore = 0
+	  for (let i = 0; i < sharedSize; i++) {
+		      if (attackers[i] > defenders[i])
+			        attackersScore++
+		      else if (attackers[i] < defenders[i])
+			        attackersScore--
+		    }
+	  attackersScore += attackers.length - defenders.length
+	  
+	  if (attackersScore === 0) {
+		      attackersScore = attackers.reduce((acc, cur) => acc + cur) - defenders.reduce((acc, cur) => acc + cur)
+		    }
+	  return attackersScore <= 0
+}
+
+// https://www.codewars.com/kata/634d0f7c562caa0016debac5
