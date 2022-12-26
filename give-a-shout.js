2a1fe20feea6e050b4ca7a0b4fb313cae44676c1
thomasxwright <wrightptw@gmail.com>
2022-12-25 22:58:10 -0600

    give a shout lev 7

---
+++ give-a-shout.js
@@ -0,0 +1,6 @@
+function madShout(sidewalk){
+	  const timeToMeeting = Math.ceil((sidewalk.indexOf('F') - sidewalk.indexOf('Y')) / 2)
+	  return `O${'i'.repeat(timeToMeeting)} F!`
+}
+
+// https://www.codewars.com/kata/63a2928176157931b3945090
