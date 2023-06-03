5b5388cc7ce0c4306691ada50a215653cdbfa6bd
thomasxwright <wrightptw@gmail.com>
2023-06-02 21:46:45 -0500

    no buts lev 7

---
+++ no-buts.js
@@ -0,0 +1,7 @@
+function noIfsNoButs(a, b) {
+	  return a > b && `${a} is greater than ${b}`
+	  || a < b && `${a} is smaller than ${b}`
+	  || a === b && `${a} is equal to ${b}`
+}
+
+// https://www.codewars.com/kata/592915cc1fad49252f000006
